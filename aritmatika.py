4080d1701b972d41d1986c800db98bb4e3cfe34a
sonhak-18 <fawaid1808@gmail.com>
2022-07-18 09:48:20 +0700

    first commit

---
+++ aritmatika.py
@@ -0,0 +1,47 @@
+a = 14
+b = 3
+
+#operasi aritmatika (+)
+hasil = a + b
+print(a, '+' ,b, '=' ,hasil)
+
+#operasi aritmatika (-)
+hasil = a - b
+print(a, '-' ,b, '=' ,hasil)
+
+#operasi aritmatika (*)
+hasil = a * b
+print(a, '*' ,b, '=' ,hasil)
+
+#operasi aritmatika (/)
+hasil = a / b
+print(a, '/' ,b, '=' ,hasil)
+
+#operasi aritmatika (pangkat)
+hasil = a ** b
+print(a, '**' ,b, '=' ,hasil)
+
+#operasi aritmatika (modulus)
+hasil = a % b
+print(a, '%' ,b, '=' ,hasil)
+
+#operasi aritmatika (floor devision)
+hasil = a // b
+print(a, '//' ,b, '=' ,hasil)
+
+#prioritas operasi 
+
+x = 3
+y = 2
+z = 4
+
+hasil = x ** y * (z + x / y) - y % z // z
+print(x,'**',y,'*',z,'+',x,'/',y,'-',y,'%',z,'//',x,'=',hasil)  
+
+hasil =  x * z - y
+print(x,'*',z,'-',y,'=',hasil)
+
+hasil =  x * (z - y)
+print(x,'*',z,'-',y,'=',hasil)
+
+
